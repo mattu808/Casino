b15a1a436a0fe90385c8f490d8f358fff1f6daab
Jianyu Li <jianyucode@gmail.com>
2019-08-22 19:35:00 -0600

    added welcome and game methods

--- casino.rb
+++ casino.rb
@@ -24,3 +24,22 @@ class Player
   end
 end
 
+def welcome_method
+  puts "Welcome to online Casino"
+  puts "\n"
+  puts "What Games would you like to play?"
+  puts "\n"
+  puts "1) Slot"
+  puts '\n'
+  puts "2) Highs / Lows"
+  game_choice_methods
+end
+
+def game_choice_methods
+  user_game_choice = gets.to_i
+  case user_game_choice
+  when 1
+    slot_game_method
+  when 2
+    high_and_low_game_method
+end
